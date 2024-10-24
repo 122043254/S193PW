ed4c557edb663d17caf28eaee0a5b8545b932b61
Lot Mondragon <139500449+122043254@users.noreply.github.com>
2024-10-24 14:16:44 -0600

    Avance practica 6

--- introLaravel/app/Http/Controllers/ControladorVistas.php
+++ introLaravel/app/Http/Controllers/ControladorVistas.php
@@ -25,13 +25,33 @@ class ControladorVistas extends Controller
 
     public function procesarCliente(Request $peticion)
     {
-        return 'Si llego la info del cliente :)';
+        //return 'Si llego la info del cliente :)';
 
         //mostramos todo lo que trae la peticion
         //return $peticion->all();
         //return $peticion->path();
         //return $peticion->url();
         //return $peticion->ip();
+        //return redirect('/'); //Redirecciona al inicio
+        //return redirect()->route('rutaClientes'); //Redirecciona a la ruta clientes
+        //return back();
+
+        //$id= [['usuario'=>1],['usuario'=>2]];
+        //return view('form',compact('id'));
+
+        $validacion= $peticion->validate([
+            'txtnombre'=> 'required|min:4|max:20',
+            'txtapellidos'=> 'required',
+            'txtcorreo'=> 'required',
+            'txttelefono'=> 'required|numeric',
+        ]);
+
+        //redireccion con un mensaje flashb
+        $usuario= $peticion->input('txtNombre');
+
+        session()->flash('exito','Se guardo el usuario: '.$usuario);
+        return to_route('rutaform');
+
     }
     
 }
--- introLaravel/resources/views/formulario.blade.php
+++ introLaravel/resources/views/formulario.blade.php
@@ -7,6 +7,21 @@
     {{-- Inicia Tarjeta con formulario --}}
 
     <div class="container mt-5 col-md-6">
+        {{-- @if(session('exito'))
+        <x-Alert tipo="success"> {{session('exito')}}</x-Alert>
+        @endif --}}
+        {{--@session('exito')
+        <x-Alert style="background-color: greenyellow;" tipo="warning" > {{ $value }}</x-Alert>
+        @endsession--}}
+        
+        @session('exito')
+        {!<script>
+            Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"});
+           </script>!}
+        @endsession
 
         <div class="card font-monospace">
 
@@ -20,19 +35,23 @@
                     @csrf
                     <div class="mb-3">
                         <label for="nombre" class="form-label">Nombre: </label>
-                        <input type="text" class="form-control" name="txtnombre">
+                        <input type="text" class="form-control" name="txtnombre" id="nombre">
+                        <small>{{ $errors->first('txtnombre') }}</small>                    
                     </div>
                     <div class="mb-3">
                         <label for="apellidos" class="form-label">Apellidos: </label>
-                        <input type="text" class="form-control" name="txtapellidos">
+                        <input type="text" class="form-control" name="txtapellidos" id="apellidos">
+                        <small>{{ $errors->first('txtapellidos') }}</small>
                     </div>
                     <div class="mb-3">
                         <label for="correo" class="form-label">Correo: </label>
-                        <input type="email" class="form-control" name="txtcorreo">
+                        <input type="text" class="form-control" name="txtcorreo">
+                        <small>{{ $errors->first('txtcorreo') }}</small>
                     </div>
                     <div class="mb-3">
                         <label for="telefono" class="form-label">Telefono: </label>
-                        <input type="number" class="form-control" name="txttelefono">
+                        <input type="text" class="form-control" name="txttelefono">
+                        <small>{{ $errors->first('txttelefono') }}</small>
                     </div>
             </div>
 

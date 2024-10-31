cd6c626ebd728dffaa56c757308cd14cf72df8cf
Lot Mondragon <139500449+122043254@users.noreply.github.com>
2024-10-30 22:29:14 -0600

    Paso 4 - Rutas

--- practicaRepaso2/routes/web.php
+++ practicaRepaso2/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','principal')->name('rutaPrincipal');
+Route::get('/registroLibro','registroLibro')->name('rutaRegistroLibro');
+Route::post('/enviarLibro','procesarLibro');

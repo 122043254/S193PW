4b128fa3bf1f3bf2805c97eabe4a66053977c587
Lot Mondragon <139500449+122043254@users.noreply.github.com>
2024-10-31 10:40:29 -0600

    Paso 7 - Validaciones y Mensaje (Alertify)

--- practicaRepaso2/app/Http/Controllers/ControladorVistas.php
+++ practicaRepaso2/app/Http/Controllers/ControladorVistas.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\validadorLibro;
 
 class ControladorVistas extends Controller
 {
@@ -14,6 +15,15 @@ class ControladorVistas extends Controller
     public function registroLibro()
     {
         return view('registroLibro');
+    }
+
+    public function procesarLibro(validadorLibro $requestV)
+    {
+        $libro = $requestV->input('txttitulo');
+
+        session()->flash('exito', 'Todo correcto: Libro "' . $libro . '" guardado');
+
+        return to_route('rutaRegistroLibro');
     }
 
 }
--- practicaRepaso2/resources/views/registroLibro.blade.php
+++ practicaRepaso2/resources/views/registroLibro.blade.php
@@ -6,48 +6,58 @@
 
 <div class="container mt-5 col-md-6">
 
+    @if (session('exito'))
+        <script>
+            Swal.fire({
+                title: '¡Todo correcto!',
+                text: '{!! session('exito') !!}',
+                icon: 'success'
+            });
+        </script>
+    @endif
+
     <div class="card shadow">
         <h5 class="card-header text-center">{{__('Registro de Libros')}}</h5>
         <div class="card-body">
-            <form action="/enviarLibro" method="POST"></form>
+            <form action="/enviarLibro" method="POST">
                 @csrf
                 <div class="mb3">
                     <label for="isbn" class="form-label">{{__('ISBN')}}</label>
                     <input type="text" class="form-control" id="isbn" name="txtisbn" value="{{old('isbn')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txtnombre') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txtisbn') }}</small>
                 </div>
                 <div class="mb3">
                     <label for="titulo" class="form-label">{{__('Título')}}</label>
                     <input type="text" class="form-control" id="titulo" name="txttitulo" value="{{old('titulo')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txttitulo') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txttitulo') }}</small>
                 </div>
                 <div class="mb3">
                     <label for="autor" class="form-label">{{__('Autor')}}</label>
                     <input type="text" class="form-control" id="autor" name="txtautor" value="{{old('autor')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txtautor') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txtautor') }}</small>
                 </div>
                 <div class="mb3">
                     <label for="paginas" class="form-label">{{__('Número de páginas')}}</label>
                     <input type="number" class="form-control" id="paginas" name="txtpaginas" value="{{old('paginas')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txtpaginas') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txtpaginas') }}</small>
                 </div>
                 <div class="mb3">
                     <label for="editor" class="form-label">{{__('Año de publicación')}}</label>
                     <input type="number" class="form-control" id="editor" name="txtaño" value="{{old('fechaPublicacion')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txtaño') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txtaño') }}</small>
                 </div>
                 <div class="mb3">
                     <label for="editor" class="form-label">{{__('Editorial')}}</label>
                     <input type="text" class="form-control" id="editor" name="txteditorial" value="{{old('editor')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txteditorial') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txteditorial') }}</small>
                 </div>
                 <div class="mb3">
-                    <label for="idioma" class="form-label">{{__('Email de Idioma')}}</label>
+                    <label for="idioma" class="form-label">{{__('Email de Editorial')}}</label>
                     <input type="text" class="form-control" id="idioma" name="txtemailEditorial" value="{{old('idioma')}}">
-                    <small class="text-danger fst-italic">{{ $error->first('txtemailEditorial') }}</small>
+                    <small class="text-danger fst-italic">{{ $errors->first('txtemailEditorial') }}</small>
                 </div>
                 <div class="d-grid mt-4">
-                    <button type="submit" class="btn btn-primary">{{__('Guardar libro')}}</button>
+                    <button type="submit" class="btn btn-primary btn-sm">{{__('Guardar libro')}}</button>
                 </div>
             </form>
         </div>
